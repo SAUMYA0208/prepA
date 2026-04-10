fcaf266b02e97e807cbf226118404ca8e47f35ca
SAUMYA0208 <156111572+SAUMYA0208@users.noreply.github.com>
2026-04-11 02:30:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
